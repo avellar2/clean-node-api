6349ed3e36663287df3e215b69aa4c25bc50aea5
Vanderson <vandimavellar1997@gmail.com>
2023-06-08 01:34:55 -0300

    chore: add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off'
   }
 }
